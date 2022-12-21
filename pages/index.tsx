c48a9cc2bcce3254040c5b07e625b1bd8afa6a4f
Edward Liu <edliu9350@gmail.com>
2022-12-21 20:43:24 +0100

    Added Search Button

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Head from 'next/head'
 import Navbar from '../components/Navbar'
+import classNames from 'classnames'
 
 export default function Home() {
   return (
@@ -12,6 +13,66 @@ export default function Home() {
       </Head>
       <main>
         <Navbar />
+        <section className="px-2 py-4 flex flex-col md:justify-center md:items-center gap-y-4">
+          <div
+            className={classNames(
+              'flex flex-col transition-all duration-300',
+              'mt-[10rem]'
+            )}
+          >
+            <form>
+              <label className="mb-2 font-medium sr-only dark:text-white">
+                Search
+              </label>
+              <div className="relative md:w-[760px]">
+                <div className="absolute inset-y-0 left-0 flex items-center pl-4 pointer-events-none">
+                  <svg
+                    aria-hidden="true"
+                    className="w-5 h-5"
+                    fill="none"
+                    stroke="currentColor"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                    ></path>
+                  </svg>
+                </div>
+                <input
+                  type="search"
+                  id="default-search"
+                  className="block w-full p-4 pl-10 border-purple-dark border-2 transition-all rounded-t-[2.2rem] bg-gray-50 focus:ring-purple-lighest focus:border-purple-lighest dark:text-white dark:focus:ring-purple-lighest dark:focus:border-purple-lighest outline-none focus:shadow-custom dark:bg-[black]"
+                  placeholder="Search for restaurants and more"
+                  required
+                  autoComplete="off"
+                />
+                <button
+                  type="submit"
+                  className="text-white absolute right-2.5 bottom-2.5 bg-purple-dark hover:bg-purple-darkest focus:ring-4 focus:outline-none focus:ring-purple-light font-medium rounded-full px-4 py-2 dark:hover:bg-purple-darkest transition"
+                >
+                  Search
+                </button>
+              </div>
+            </form>
+            <div className="flex items-center gap-x-3 md:gap-x-4 justify-end bg-purple-so-lighest md:w-[760px] rounded-b-[2.2rem] dark:bg-purple-dark">
+              <p className="text-[1.1rem] dark:text-white">
+                <span role={'img'}>📍</span> Location
+              </p>
+              <input
+                type="text"
+                className="w-48 md:w-auto p-3 pl-5 border-b-2 border-l-2 border-r-2 border-purple-dark text-[1.1rem] rounded-br-[2.2rem] focus:ring-purple-lighest focus:border-purple-lighest outline-none bg-white dark:bg-[black] transition"
+                placeholder="City"
+              />
+            </div>
+          </div>
+          <div>Searching</div>
+          <div>Allow Location Access</div>
+          <div>Search Results</div>
+        </section>
       </main>
     </>
   )
